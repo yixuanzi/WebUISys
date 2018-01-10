a07d27da88658535a2b1e1ca71f1ea14535c5999
shadow4N <guoguisheng@8lab.cn>
2018-01-10 19:22:59 +0800

    update extension

---
+++ main.js
@@ -0,0 +1,4 @@
+window.htmlParser = require("htmlparser2-without-node-native");
+window.cheerio = require("cheerio-without-node-native");
+window.nodefs = require('fs');
+window.nodeiconv = require('iconv-lite');
